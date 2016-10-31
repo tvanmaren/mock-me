0190a06a58bdda294dc9b557d1b8becdd5c4c2f9
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-10-31 17:12:34 -0600

    first Modal sets ipsum data to localStorage, then passes along to second Modal

--- script.js
+++ script.js
@@ -2,8 +2,43 @@
 
 
 $(function() {
+    function getIpsum(event) {
+        var ipsum = $(event.target).text();
+        switch (ipsum) {
+            case 'Hipster':
+                {
+                    console.log('you\'ve chosen the hipster Ipsum');
+                    $.getJSON('http://hipsterjesus.com/api/', function(hipsterGoodness) {
+                        if (hipsterGoodness.text.length) {
+                            console.log('storing', hipsterGoodness);
+                            localStorage.setItem('ipsum', hipsterGoodness.text);
+                            console.log('stored', localStorage.getItem('ipsum'));
+                            $('#setupModal').modal('hide');
+                        }
+                    });
+                    break;
+                }
+            default:
+                {
+                    console.log('you didn\'t choose an ipsum');
+                    return;
+                }
+        }
+    }
+
+    function getWatsonInfo() {
+        console.log('ready for Watson!');
+        $('#apiModal').modal();
+        //Watson getJSON goes here
+        //Then pass to getShutterStockPhotos
+    }
+
     function setup() {
+        //insert page styling & setup here//
         $('#setupModal').modal();
     }
     setup();
-  });
+    $('#hipster').click(getIpsum);
+    console.log('well, almost there.');
+    $('#setupModal').on('hide.bs.modal', getWatsonInfo);
+});
