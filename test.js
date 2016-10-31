93ea46f4b39a9a79c11268be521e224425847508
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-10-31 11:37:18 -0600

    setup basic project information & testing -- API's not working yet

---
+++ test.js
@@ -0,0 +1,33 @@
+'use strict';
+// console.log('Hipster Jesus',$.getJSON('http://hipsterjesus.com/api/', function(data) {
+    // $('#content').html( data.text );
+// }));
+
+var url1 = 'https://g-watson.herokuapp.com/tone-analyzer/api/v3/tone?version=2016-05-19&text=';
+
+// var text=$.getJSON('http://hipsterjesus.com/api/');
+var text="Hallelujah";
+
+console.log(url1);
+
+var USERNAME="d7866d3a-cfa3-419f-89a5-20abd7b7eee5";
+var PASSWORD="HebDAFtfkJSb";
+
+// var text = $.ajax({headers: {
+  //  "Authorization": "Basic " + btoa(USERNAME + ":" + PASSWORD)},
+  // dataType: "json",  url: url1+text});
+
+var text=$.getJSON(url1+text);
+
+console.log(text);
+
+text.done(console.log(this));
+
+// var USERNAME="4ec1e4604d0df001e322";
+// var PASSWORD="e079a0cfeb1147c55ac1d6d1ecaf2561b60def1c";
+// $.ajaxSetup({
+//   headers: {
+//       "Authorization": "Basic " + btoa(USERNAME + ":" + PASSWORD)
+//     }
+// });
+// console.log('shutterstock photos,', $.getJSON("https://4ec1e4604d0df001e322:e079a0cfeb1147c55ac1d6d1ecaf2561b60def1c@api.shutterstock.com/v2/images/search?query=APPLE"));
