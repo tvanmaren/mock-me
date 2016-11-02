836f63ce9e596efd003a6c6c0b6c9e9a16f347ad
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-11-02 13:33:11 -0600

    add music(random), carousel, play, pause, and reset functionality

--- presentation.js
+++ presentation.js
@@ -1,9 +1,82 @@
 'use strict';
 
-$(function() {
-  function streamImages() {
-    var images=JSON.parse(localStorage.getItem('images'));
-    
-  }
+function resetPage() {
+    $('body #myCarousel').remove();
+    $('audio').remove();
+    setupCarousel();
+    setup();
+    $('#hipster').click(getIpsum);
+}
 
+function setupCarousel() {
+    //create Carousel//
+    $('body').append('<div id="myCarousel" class="carousel slide" data-ride="carousel"><ol class="carousel-indicators"></ol><div class="carousel-inner" role="listbox"></div></div>');
+    //add left control//
+    $('#myCarousel').append('<a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="icon-prev"></span></a>');
+    //add right control//
+    $('#myCarousel').append('<a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="icon-next"></span></a>');
+    //add Play/Pause button//
+    $('#myCarousel').append('<div id="carouselButtons"><button id="pausePlayButton" type="button" class="btn btn-default btn-xl"><span class="glyphicon glyphicon-pause"></span></button></div>');
+    $('#pausePlayButton').click(playPause);
+    //add Reset button//
+    $('#carouselButtons').append('<button id="resetButton" type="button" class="btn btn-default btn-xl"><span class="glyphicon glyphicon-refresh"></span></button>');
+    $('#resetButton').click(resetPage);
+}
+
+function playPause() {
+    var $button = $('#pausePlayButton span');
+    if ($button.hasClass('glyphicon-play')) {
+        $button.removeClass('glyphicon-play');
+        $button.addClass('glyphicon-pause');
+        $('#myCarousel').carousel('cycle');
+        $('audio').get(0).play();
+        return;
+    } else if ($button.hasClass('glyphicon-pause')) {
+        $button.removeClass('glyphicon-pause');
+        $button.addClass('glyphicon-play');
+        $('#myCarousel').carousel('pause');
+        $('audio').get(0).pause();
+        return;
+    }
+}
+
+function streamImages() {
+    var slideIndicator, slideWrapper, slideCaption, slideHead, slideText;
+    var images = JSON.parse(localStorage.getItem('images'));
+    var ipsum = localStorage.getItem('ipsum').split(' ');
+    for (let i = 0; i < images.length; i++) {
+        //setup the slide itself//
+        slideIndicator = '<li data-target="#myCarousel" data-slide-to=' + i + '></li>';
+        $('.carousel-indicators').append(slideIndicator);
+
+        //grab ipsum caption//
+        slideHead = ipsum.slice((i * 5), (5 + i * 5)).join(' ');
+        console.log(slideHead);
+
+        slideText = '';
+        // slideText = images[i].description; //too much info here
+
+        //setup the display image & text//
+        slideCaption = '<div class="carousel-caption"><h1>' + slideHead + '</h1><p>' + slideText + '</p></div>';
+        slideWrapper = '<div class="item"><img class=img-responsive src=' + images[i].assets.preview.url + ' alt=' + images[i].image_type + '>' + slideCaption + '</div>';
+        $('.carousel-inner').append(slideWrapper);
+    }
+    $('.carousel-indicators :first-child').addClass('active');
+    $('.carousel-inner :first-child').addClass('active');
+    $('#myCarousel').carousel({
+        interval: 3000,
+        pause: "false"
+    });
+}
+
+function streamMusic() {
+    var music = JSON.parse(localStorage.getItem('music'));
+    $('body').append('<audio controls autoplay></audio>');
+    var toStream = music[Math.floor((Math.random() * music.length))];
+    $('audio').append('<span>'+toStream.description+'</span>');
+    $('audio').append('<source src="' + toStream.assets.preview_ogg.url + '" type="audio/ogg"><source src="' + toStream.assets.preview_mp3.url + '" type="audio/mpeg">');
+}
+
+$(function() {
+    setupCarousel();
 });
--- script.js
+++ script.js
@@ -1,144 +1,151 @@
 'use strict';
 
+const options = {
+    backdrop: 'static',
+    keyboard: false
+};
+const watsonURL = "http://g-watson-tristan.herokuapp.com/?text=";
+const ipsumURL = 'http://hipsterjesus.com/api/?type=hipster-centric'; //probably store this in the function, not here
+const imageURL = "https://4ec1e4604d0df001e322:e079a0cfeb1147c55ac1d6d1ecaf2561b60def1c@api.shutterstock.com/v2/images/search?query=";
+const musicURL = "https://4ec1e4604d0df001e322:e079a0cfeb1147c55ac1d6d1ecaf2561b60def1c@api.shutterstock.com/v2/audio/search?query=";
+const videoURL = "https://4ec1e4604d0df001e322:e079a0cfeb1147c55ac1d6d1ecaf2561b60def1c@api.shutterstock.com/v2/videos/search?query=";
 
+function getIpsum(event) {
+    var ipsum = $(event.target).text();
+    switch (ipsum) {
+        case 'Hipster':
+            {
+                console.log('you\'ve chosen the hipster Ipsum');
+                $.getJSON(ipsumURL, function(hipsterGoodness) {
+                    if (hipsterGoodness.text.length) {
+                        console.log('storing', hipsterGoodness);
+                        localStorage.setItem('ipsum', hipsterGoodness.text);
+                        console.log('stored', localStorage.getItem('ipsum'));
+                        $('#ipsumModal').modal('hide');
+                        $('#apiModal').modal(options);
+                        $('#analyze').removeClass('disabled');
+                    }
+                });
+                break;
+            }
+        default:
+            {
+                console.log('you didn\'t choose an ipsum');
+                return;
+            }
+    }
+    $('#analyze').click(getWatsonInfo);
+    return;
+}
+
+function getWatsonInfo() {
+    if ($('#analyze').hasClass('disabled')) {
+        console.log('button disabled');
+        return;
+    } else {
+        $('#analyze').addClass('disabled');
+    }
+    var text = localStorage.getItem('ipsum');
+    //Watson getJSON goes here
+    $.getJSON(watsonURL + text, function(toneAnalysis) {
+        if (toneAnalysis.sentences_tone.length) {
+            console.log('storing', toneAnalysis);
+            localStorage.setItem('sentenceTones', JSON.stringify(toneAnalysis.sentences_tone));
+            localStorage.setItem('overallTone', JSON.stringify(toneAnalysis.document_tone.tone_categories));
+            console.log('stored sentences', localStorage.getItem('sentenceTones'));
+            console.log('stored document', localStorage.getItem('overallTone'));
+            $('#images').removeClass('disabled');
+        }
+    });
+    //Then enable listener for getShutterStockPhotos
+    $('#images').click(getShutterStockPhotos);
+    return;
+}
+
+function getShutterStockPhotos() {
+    //check if button is disabled or not
+    if ($('#images').hasClass('disabled')) {
+        console.log('button disabled');
+        return;
+    } else {
+        $('#images').addClass('disabled');
+    }
+    //PARSE Watson data for category here
+    var category = 'nature';
+    //PARSE Watson data for category here
+    var USERNAME = "4ec1e4604d0df001e322";
+    var PASSWORD = "e079a0cfeb1147c55ac1d6d1ecaf2561b60def1c";
+    $.ajaxSetup({
+        headers: {
+            "Authorization": "Basic " + btoa(USERNAME + ":" + PASSWORD)
+        }
+    });
+    $.getJSON(imageURL + category, function(imageData) {
+        if (imageData.data.length) {
+            console.log('storing', imageData);
+            localStorage.setItem('images', JSON.stringify(imageData.data));
+            console.log('stored', localStorage.getItem('images'));
+            $('#music').removeClass('disabled');
+        }
+    });
+    //Then enable listener for getShutterStockMusic
+    $('#music').click(getShutterStockMusic);
+    return streamImages();
+}
+
+function getShutterStockMusic() {
+    //check if button is disabled or not
+    if ($('#music').hasClass('disabled')) {
+        return;
+    } else {
+        $('#music').addClass('disabled');
+    }
+    //PARSE Watson data for category here
+    var category = 'disgust';
+    //PARSE Watson data for category here
+    $.getJSON(musicURL + category, function(musicData) {
+        if (musicData.data.length) {
+            console.log('storing', musicData);
+            localStorage.setItem('music', JSON.stringify(musicData.data));
+            console.log('stored', localStorage.getItem('music'));
+            $('#video').removeClass('disabled');
+        }
+    });
+    //Then enable listener for getShutterStockVideo
+    $('#video').click(getShutterStockVideo);
+    return streamMusic();
+}
+
+function getShutterStockVideo() {
+    //check if button is disabled or not
+    if ($('#video').hasClass('disabled')) {
+        return;
+    } else {
+        $('#video').addClass('disabled');
+    }
+    //PARSE Watson data for category here
+    var category = 'nature';
+    //PARSE Watson data for category here
+    $.getJSON(videoURL + category, function(videoData) {
+        if (videoData.data.length) {
+            console.log('storing', videoData);
+            localStorage.setItem('video', JSON.stringify(videoData.data));
+            console.log('stored', localStorage.getItem('video'));
+            $('#apiModal').modal('hide');
+            //pass to slideshow here
+            return;
+        }
+    });
+    return;
+}
+
+function setup() {
+    //insert page styling & setup here//
+    $('#ipsumModal').modal(options);
+}
+
+//Run this once the DOM is ready
 $(function() {
-    const options = {
-        backdrop: 'static',
-        keyboard: false
-    };
-    const watsonURL = "http://g-watson-tristan.herokuapp.com/?text=";
-    const ipsumURL = "http://hipsterjesus.com/api/"; //probably store this in the function, not here
-    const imageURL = "https://4ec1e4604d0df001e322:e079a0cfeb1147c55ac1d6d1ecaf2561b60def1c@api.shutterstock.com/v2/images/search?query=";
-    const musicURL = "https://4ec1e4604d0df001e322:e079a0cfeb1147c55ac1d6d1ecaf2561b60def1c@api.shutterstock.com/v2/audio/search?query=";
-    const videoURL = "https://4ec1e4604d0df001e322:e079a0cfeb1147c55ac1d6d1ecaf2561b60def1c@api.shutterstock.com/v2/videos/search?query=";
-
-    function getIpsum(event) {
-        var ipsum = $(event.target).text();
-        switch (ipsum) {
-            case 'Hipster':
-                {
-                    console.log('you\'ve chosen the hipster Ipsum');
-                    $.getJSON(ipsumURL, function(hipsterGoodness) {
-                        if (hipsterGoodness.text.length) {
-                            console.log('storing', hipsterGoodness);
-                            localStorage.setItem('ipsum', hipsterGoodness.text);
-                            console.log('stored', localStorage.getItem('ipsum'));
-                            $('#ipsumModal').modal('hide');
-                            $('#apiModal').modal(options);
-                            $('#analyze').removeClass('disabled');
-                        }
-                    });
-                    break;
-                }
-            default:
-                {
-                    console.log('you didn\'t choose an ipsum');
-                    return;
-                }
-        }
-        $('#analyze').click(getWatsonInfo);
-        return;
-    }
-
-    function getWatsonInfo() {
-        if ($('#analyze').hasClass('disabled')) {
-            console.log('button disabled');
-            return;
-        }
-        var text = localStorage.getItem('ipsum');
-        //Watson getJSON goes here
-        $.getJSON(watsonURL + text, function(toneAnalysis) {
-            if (toneAnalysis.sentences_tone.length) {
-                console.log('storing', toneAnalysis);
-                localStorage.setItem('sentenceTones', JSON.stringify(toneAnalysis.sentences_tone));
-                localStorage.setItem('overallTone', JSON.stringify(toneAnalysis.document_tone.tone_categories));
-                console.log('stored sentences', localStorage.getItem('sentenceTones'));
-                console.log('stored document', localStorage.getItem('overallTone'));
-                $('#images').removeClass('disabled');
-            }
-        });
-        //Then enable listener for getShutterStockPhotos
-        $('#images').click(getShutterStockPhotos);
-        return;
-    }
-
-    function getShutterStockPhotos() {
-        //check if button is disabled or not
-        if ($('#images').hasClass('disabled')) {
-            console.log('button disabled');
-            return;
-        }
-        //PARSE Watson data for category here
-        var category = 'nature';
-        //PARSE Watson data for category here
-        var USERNAME = "4ec1e4604d0df001e322";
-        var PASSWORD = "e079a0cfeb1147c55ac1d6d1ecaf2561b60def1c";
-        $.ajaxSetup({
-            headers: {
-                "Authorization": "Basic " + btoa(USERNAME + ":" + PASSWORD)
-            }
-        });
-        $.getJSON(imageURL + category, function(imageData) {
-            if (imageData.data.length) {
-                console.log('storing', imageData);
-                localStorage.setItem('images', JSON.stringify(imageData.data));
-                console.log('stored', localStorage.getItem('images'));
-                $('#music').removeClass('disabled');
-            }
-        });
-        //Then enable listener for getShutterStockMusic
-        $('#music').click(getShutterStockMusic);
-        return;
-    }
-
-    function getShutterStockMusic() {
-        //check if button is disabled or not
-        if ($('#music').hasClass('disabled')) {
-            return;
-        }
-        //PARSE Watson data for category here
-        var category = 'nature';
-        //PARSE Watson data for category here
-        $.getJSON(musicURL + category, function(musicData) {
-            if (musicData.data.length) {
-                console.log('storing', musicData);
-                localStorage.setItem('music', JSON.stringify(musicData.data));
-                console.log('stored', localStorage.getItem('music'));
-                $('#video').removeClass('disabled');
-            }
-        });
-        //Then enable listener for getShutterStockVideo
-        $('#video').click(getShutterStockVideo);
-        return;
-    }
-
-    function getShutterStockVideo() {
-        //check if button is disabled or not
-        if ($('#music').hasClass('disabled')) {
-            return;
-        }
-        //PARSE Watson data for category here
-        var category = 'nature';
-        //PARSE Watson data for category here
-        $.getJSON(videoURL + category, function(videoData) {
-            if (videoData.data.length) {
-                console.log('storing', videoData);
-                localStorage.setItem('video', JSON.stringify(videoData.data));
-                console.log('stored', localStorage.getItem('video'));
-                $('#apiModal').modal('hide');
-                //pass to slideshow here
-            }
-        });
-        return;
-    }
-
-    function setup() {
-        //insert page styling & setup here//
-        $('#ipsumModal').modal(options);
-    }
-
-    //Run this once the DOM is ready
-
     setup();
     $('#hipster').click(getIpsum);
     // $('#ipsumModal').on('hidden.bs.modal', getWatsonInfo); //use this method in order to automate (hide) the watson API call
