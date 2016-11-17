b1e27e9698d70670f78ee4e0b3c5112c86b37f3c
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-11-16 23:20:22 -0700

    fix html injection formatting

--- js/setup.js
+++ js/setup.js
@@ -166,14 +166,53 @@ function getShutterStockVideo() {
 }
 
 function insertIpsumModal() {
-    $('.container-fluid').append('<div class="modal fade bs-example-modal-md" tabindex="-1" role="dialog" id="ipsumModal"><div class="modal-dialog modal-md" role="document"><div class="modal-content"><h2 class="modal-title text-center">Today, I feel like a...</h2><div class="modal-body"><div class="text-center center-block"><div class="row"><div class="btn-group btn-group-vertical"><button id="hipster" class="btn btn-info btn-lg">Hipster</button><button id="pony" class="btn btn-info btn-lg">Pony</button><button id="dino" class="btn btn-info btn-lg">Dino</button></div><div class="btn-group btn-group-vertical"><button class="btn btn-link btn-block"></button><button class="btn btn-link"></button><button class="btn btn-link"></button></div><div class="btn-group btn-group-vertical"><button id="bacon" class="btn btn-info btn-lg">Pig</button><button id="skater" class="btn btn-danger btn-lg disabled">Skater</button><button id="custom" class="btn btn-lg btn-block white-background text-warning" autocomplete="off"><b>Customizer...</b></button></div></div></div><div class="row text-right"><button id="reset" class="btn btn-warning btn-xs"><small class="text-center"><div>reset</div><div>all</div>slides</div></small></button></div></div></div></div></div>');
+    $('.container-fluid').append(`
+      <div class="modal fade bs-example-modal-md" tabindex="-1" role="dialog" id="ipsumModal">
+        <div class="modal-dialog modal-md" role="document">
+          <div class="modal-content">
+            <h2 class="modal-title text-center">Today, I feel like a...</h2>
+            <div class="modal-body">
+              <div class="text-center center-block">
+                <div class="row">
+                  <div class="btn-group btn-group-vertical"><button id="hipster" class="btn btn-info btn-lg">Hipster</button><button id="pony" class="btn btn-info btn-lg">Pony</button><button id="dino" class="btn btn-info btn-lg">Dino</button></div>
+                  <div class="btn-group btn-group-vertical"><button class="btn btn-link btn-block"></button><button class="btn btn-link"></button><button class="btn btn-link"></button></div>
+                  <div class="btn-group btn-group-vertical"><button id="bacon" class="btn btn-info btn-lg">Pig</button><button id="skater" class="btn btn-danger btn-lg disabled">Skater</button><button id="custom" class="btn btn-lg btn-block white-background text-warning" autocomplete="off"><b>Customizer...</b></button></div>
+                </div>
+              </div>
+              <div class="row text-right">
+                <button id="reset" class="btn btn-warning btn-xs">
+                  <small class="text-center">
+                    <div>reset</div>
+                    <div>all</div>
+                    <div>slides</div>
+                  </small>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `);
     return;
 }
 
 function insertAPIModal() {
-    $('.container-fluid').append(
-        '<div class="modal fade bs-example-modal-md" tabindex="- 1" role="dialog" id="apiModal"><div class="modal-dialog modal-md" role="document"><div class="modal-content"><h2 class="modal-title text-center">Click to fetch your presentation</h2><div class="modal-body"><div class="text-center center-block"><div class="btn-group btn-group-vertical"><button id="analyze" class="btn btn-danger btn-lg disabled process"><span class="glyphicon glyphicon-cog"></span><div>Crunch Data</div></button><button class="btn btn-link process"><span class="glyphicon glyphicon-arrow-down"></span></button><button id="images" class="btn btn-danger btn-lg disabled process"><span class = "glyphicon glyphicon-picture"></span><div>Laminate Images</div></button></div><div class="btn-group btn-group-vertical"><button class="btn btn-link process"></button><button class="btn btn-link process"></button><button class="btn btn-link process"><span class="glyphicon glyphicon-arrow-right"></span></button></div><div class="btn-group btn-group-vertical"><button id="video" class="btn btn-danger btn-lg disabled process"><span class = "glyphicon glyphicon-film"></span><div>Clip Video</div></button><button class="btn btn-link process"><span class="glyphicon glyphicon-arrow-up"></span></button><button id="music" class="btn btn-danger btn-lg disabled process"><span class = "glyphicon glyphicon-music"></span><div>Pipe Music</div></button></div></div></div></div></div></div>'
-    );
+    $('.container-fluid').append(`
+      <div class="modal fade bs-example-modal-md" tabindex="- 1" role="dialog" id="apiModal">
+      <div class="modal-dialog modal-md" role="document">
+        <div class="modal-content">
+          <h2 class="modal-title text-center">Click to fetch your presentation</h2>
+          <div class="modal-body">
+            <div class="text-center center-block">
+              <div class="btn-group btn-group-vertical"><button id="analyze" class="btn btn-danger btn-lg disabled process"><span class="glyphicon glyphicon-cog"></span><div>Crunch Data</div></button><button class="btn btn-link process"><span class="glyphicon glyphicon-arrow-down"></span></button><button id="images" class="btn btn-danger btn-lg disabled process"><span class = "glyphicon glyphicon-picture"></span><div>Laminate Images</div></button></div>
+              <div class="btn-group btn-group-vertical"><button class="btn btn-link process"></button><button class="btn btn-link process"></button><button class="btn btn-link process"><span class="glyphicon glyphicon-arrow-right"></span></button></div>
+              <div class="btn-group btn-group-vertical"><button id="video" class="btn btn-danger btn-lg disabled process"><span class = "glyphicon glyphicon-film"></span><div>Clip Video</div></button><button class="btn btn-link process"><span class="glyphicon glyphicon-arrow-up"></span></button><button id="music" class="btn btn-danger btn-lg disabled process"><span class = "glyphicon glyphicon-music"></span><div>Pipe Music</div></button></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `);
     return;
 }
 
