4b0c0b2944023c717e6db53e1b9cdb898c1366db
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-03-07 02:15:19 -0700

    test querystring module

--- index.js
+++ index.js
@@ -7,14 +7,20 @@ if (process.env.NODE_ENV !== 'production') {
 // const b64=require('btoa');
 const express = require('express');
 const axios = require('axios');
+const querystring = require('querystring');
 const WATSON_USERNAME = process.env.WATSON_USERNAME;
 const WATSON_PASSWORD = process.env.WATSON_PASSWORD;
 // const SHUTTERSTOCK_ID = process.env.SHUTTERSTOCK_ID;
 // const SHUTTERSTOCK_SECRET = process.env.SHUTTERSTOCK_SECRET;
 // const Authorization=`Basic ${b64(`${SHUTTERSTOCK_ID}:${SHUTTERSTOCK_SECRET}`)}`;
 // const SHUTTERSTOCK_HEADERS={headers: { Authorization }};
-const access_token=process.env.SHUTTERSTOCK_TOKEN;
-const SHUTTERSTOCK_HEADERS={headers: { "Authorization": "Bearer: "+access_token, "User-Agent": null}};
+const access_token = process.env.SHUTTERSTOCK_TOKEN;
+const SHUTTERSTOCK_HEADERS = {
+  headers: {
+    "Authorization": "Bearer: " + access_token,
+    "User-Agent": null
+  }
+};
 
 var app = express();
 
@@ -29,14 +35,15 @@ var tone_analyzer = watson.tone_analyzer({
 
 app.use(express.static('./public'));
 
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");    res.setHeader("Access-Control-Allow-Credentials", "true");
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
   next();
 });
 
-app.get('/ipsum/:type', function(req,res){
+app.get('/ipsum/:type', function (req, res) {
   const type = req.params.type;
 
   const ipsumDict = {
@@ -47,7 +54,11 @@ app.get('/ipsum/:type', function(req,res){
     pig: 'https://baconipsum.com/api/?type=just-meat'
   };
 
-  axios.get(ipsumDict[type], {headers: {"responseType": "json"}})
+  axios.get(ipsumDict[type], {
+      headers: {
+        "responseType": "json"
+      }
+    })
     .then((result) => {
       res.json(result.data);
     })
@@ -56,38 +67,47 @@ app.get('/ipsum/:type', function(req,res){
     });
 });
 
-app.get('/watson', function(req,res){
-  console.log('watson query:',req.query);
+app.get('/watson', function (req, res) {
+  console.log('watson query:', req.query);
   console.log("tone analyzer");
-    tone_analyzer.tone({ text: req.query.text },
-    function(err, tone) {
-      if (err){
+  tone_analyzer.tone({
+      text: req.query.text
+    },
+    function (err, tone) {
+      if (err) {
         console.log(err);
-      }
-      else {
+      } else {
         res.json(tone);
       }
-  });
+    });
 });
 
-app.get('/images/', function(req,res){
+app.get('/images/', function (req, res) {
   const getPage = req.query.getPage;
   const category = req.query.category;
 
   axios.defaults.headers.common['Authorization'] = access_token;
   console.log(axios.defaults.headers);
-  const imageURL = `https://api.shutterstock.com/v2/images/search?image_type=photo&license=commercial&page=${getPage}&orientation=horizontal&sort=random&view=full&query=${category}`;
-  axios.get(imageURL)
+  const imageURL = `https://api.shutterstock.com/v2/images/search`;
+  axios.get(imageURL, querystring.stringify({
+      'image_type': 'photo',
+      'license': 'commercial',
+      'orientation': 'horizontal',
+      'sort': 'random',
+      'view': 'full',
+      'page': getPage,
+      'query': category
+    }))
     .then((result) => {
-      console.log('images:',result.data);
-      res.json(data);
+      console.log('images:', result.data);
+      res.json(result.data);
     })
     .catch((err) => {
-      console.log('ERROR:',err);
+      console.log('ERROR:', err);
     });
 });
 
-app.get('/audio/', function(req,res){
+app.get('/audio/', function (req, res) {
   console.log(req.query);
   const category = req.query.category;
 
@@ -102,7 +122,7 @@ app.get('/audio/', function(req,res){
     });
 });
 
-app.get('/videos/', function(req,res){
+app.get('/videos/', function (req, res) {
   console.log(req.query);
   const category = req.query.category;
 
