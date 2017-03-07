3d17b909c7e3caf98c84b702c3c1c31966585e54
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-03-06 22:09:37 -0700

    :sparkles: add shutterstock calls to server

--- index.js
+++ index.js
@@ -2,10 +2,15 @@
 
 require('dotenv').config();
 
-var express = require('express');
-var WATSON_URL = "https://gateway.watsonplatform.net/tone-analyzer/api";
-var WATSON_PWD = "bQ4ws7wCgfFC";
-var WATSON_USR = "34d65e1a-374f-4ca4-8a99-1f535a9c33e4";
+const express = require('express');
+const axios = require('axios');
+const WATSON_URL = "https://gateway.watsonplatform.net/tone-analyzer/api";
+const WATSON_PWD = "bQ4ws7wCgfFC";
+const WATSON_USR = "34d65e1a-374f-4ca4-8a99-1f535a9c33e4";
+
+const musicURL = "https://clientID:clientSecret@api.shutterstock.com/v2/audio/search?query=";
+
+const videoURL = "https://clientID:clientSecret@api.shutterstock.com/v2/videos/search?query=";
 
 var app = express();
 
@@ -25,7 +30,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/', function(req,res){
+app.get('/watson', function(req,res){
   console.log(req.query);
   console.log("tone analyzer");
     tone_analyzer.tone({ text: req.query.text },
@@ -37,6 +42,46 @@ app.get('/', function(req,res){
         res.json(tone);
       }
   });
+});
+
+app.get('/images/', function(req,res){
+  console.log(req.query);
+  const getPage = req.query.getPage;
+  const category = req.query.category;
+  const imageURL = `https://clientID:clientSecret@api.shutterstock.com/v2/images/search?image_type=photo&license=commercial&page=${getPage}&orientation=horizontal&sort=random&view=full&query=${category}`;
+  axios.get(imageURL)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/audio/', function(req,res){
+  console.log(req.query);
+  const category = req.query.category;
+  const musicURL = `https://clientID:clientSecret@api.shutterstock.com/v2/audio/search?query=${category}`;
+  axios.get(musicURL)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get('/videos/', function(req,res){
+  console.log(req.query);
+  const category = req.query.category;
+  const videoURL = `https://clientID:clientSecret@api.shutterstock.com/v2/videos/search?query=${category}`;
+  axios.get(videoURL)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen(process.env.PORT || 3000);
